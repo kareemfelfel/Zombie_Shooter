fcde5d61bdd769467251c136201bd7e8ab8d781a
kareemfelfel <k.a.ahmed@eagle.clarion.edu>
2020-04-06 00:33:27 -0400

    Added shooting Feature to player

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float HitPoints = 100f;
+    public void TakeDamage(float PDamage)
+    {
+        float TempHP = HitPoints - PDamage;
+        if (!(TempHP <= 0))
+            HitPoints = TempHP;
+        else
+        {
+            HitPoints = 0;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float Damage = 30f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit Hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out Hit, range))
+        {
+            Debug.Log("I did hit " + Hit.transform.name);
+            EnemyHealth target = Hit.transform.GetComponent<EnemyHealth>();
+            target.TakeDamage(Damage);
+        }
+        else
+            return;
+        
+    }
+}
