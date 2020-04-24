fd5a4e59831cc66e867a622e3e786ad87f35c35d
kareemfelfel <k.a.ahmed@eagle.clarion.edu>
2020-04-23 20:23:27 -0400

    Finished game with target rescue created

---
+++ Assets/Scripts/HostageAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HostageAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    
+    float DistanceToTarget = Mathf.Infinity;
+    UnityEngine.AI.NavMeshAgent navMeshAgent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DistanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (DistanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            //Player.getComponent<RescueHandler>.HandleWin();
+            //target.GetComponentInChildren<RescueHandler>().HandleWin();
+            target.GetComponent<RescueHandler>().HandleWin();
+            
+
+        }
+    }
+}
---
+++ Assets/Scripts/RescueHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RescueHandler : MonoBehaviour
+{
+    [SerializeField] Canvas WinCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WinCanvas.enabled = false;
+    }
+
+    // function that pops up the winning canvas
+    public void HandleWin()
+    {
+        WinCanvas.enabled = true;
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
